694558eedcceae127c23b8fb2278a9303955bbe4
yu-xiaomeng <414017727@qq.com>
2021-12-03 16:52:46 +0800

    add test_bill_details_get_by_id

--- apitest-python-pytest-requests/operation/bill_details.py
+++ apitest-python-pytest-requests/operation/bill_details.py
@@ -34,3 +34,24 @@ def bill_details_create(category_id, type, amount, note, date, token):
     result.response = res
     return result
 
+def bill_details_get_by_id(id, token):
+    """
+    记一笔
+    :param id: 账单明细id
+    :return: 自定义的关键字返回结果 result
+    """
+    result = ResultBase
+    
+    header = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token
+    }
+    res = bill_details.get_bill_details_by_id(id, headers=header)
+    result.success = False
+    if res.json()["code"] == 100:
+        result.success = True
+    else:
+        result.error = "接口返回码是 【 {} 】, 返回信息：{} ".format(res.json()["code"], res.json()["message"])
+    result.message = res.json()["message"]
+    result.response = res
+    return result
